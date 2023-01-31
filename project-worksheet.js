44e742facd770d25e881e927d03ac89d997dd02d
Bakyalakshmi <116268707+bakya236@users.noreply.github.com>
2023-01-31 19:18:10 +0530

    created project- worksheet.js
    js file is created to practice calculations related to my project

---
+++ project-worksheet.js
@@ -0,0 +1,18 @@
+//////
+
+
+//to find the current value//
+
+
+// let actual = "100"
+// let value = "10"
+// let type = "%"
+// let current;
+// if (type == "rs") {
+//     current = actual - value;
+//     console.log("current:" + current)
+// }
+// else {
+//     current = actual - actual * value / 100
+//     console.log("current:" + current);
+// }
